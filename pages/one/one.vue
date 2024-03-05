a52a2c7098eadc93cd0be970ba2f65af3f54f931
baiyuemei <superstart01@163.com>
2024-03-05 11:42:51 +0800

    初始化提交

---
+++ pages/one/one.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		uni-app开发了
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
